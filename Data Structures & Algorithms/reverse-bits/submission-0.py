4e90d7452d4f61451f3689de2b1b682535649ffb
agao323 <agao323@gmail.com>
2026-08-04 19:34:42 -0400

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        """
+        logic:
+        - turn into string, reverse, return as binary
+        """
+
+        n_str = bin(n)[::-1]
+        cleaned = n_str[:-2]
+        result = cleaned + ''.join('0' for _ in range(32 - len(cleaned)))
+        return int(result, base=2)
