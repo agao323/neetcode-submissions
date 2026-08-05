456f664cb2bc5aac96c7669f8c2f1a962b9b33c0
agao323 <agao323@gmail.com>
2026-08-04 23:37:01 -0400

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        """
+        TIME: 25:07.40 for initial naive logic with sets:
+            - iterate through entire grid once
+            - track boxes by tuples (i//3, j//3)
+
+        logic:
+        - use sets to track each row, column, and 3x3
+        - don't think there's much more to optimize?
+        - slight optimizations: 
+            - skip any empty row/column/box
+            - skip any with only one value
+        """
+
+        rows = defaultdict(set)
+        columns = defaultdict(set)
+        boxes = defaultdict(set)
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                digit = board[i][j]
+                if digit == '.':
+                    continue
+
+                if digit in rows[i]:
+                    return False
+                if digit in columns[j]:
+                    return False
+                k = ((i // 3), (j // 3))
+                if digit in boxes[k]:
+                    return False
+                
+                rows[i].add(digit)
+                columns[j].add(digit)
+                boxes[k].add(digit)
+
+        return True
