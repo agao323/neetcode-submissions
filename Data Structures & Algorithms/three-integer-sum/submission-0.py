1be98eeb21ade1096798af882666872a2109ccd5
agao323 <agao323@gmail.com>
2026-08-06 15:52:44 -0400

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.py
@@ -0,0 +1,42 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """
+        brute force:
+            - iterate 3x. O(n^3)
+        
+        better:
+            - sort. O(nlogn)
+            - for each element:
+                - two sum
+            - O(n^2)
+            - how to figure out distinct?
+        
+        can we do better?
+            - no
+
+        [-1,0,1,2,-1,-4]
+        [-4,-1,-1,0,1,2]
+        """
+        result = []
+        seen = set()
+
+        nums = sorted(nums)
+        for i in range(len(nums)):
+            target = 0 - nums[i]
+            for j in range(i + 1, len(nums)):
+                l, r = j, len(nums) - 1
+                while l < r:
+                    total = nums[l] + nums[r]
+                    if total == target:
+                        if (nums[i], nums[l], nums[r]) in seen:
+                            break
+                        result.append([nums[i], nums[l], nums[r]])
+                        seen.add((nums[i], nums[l], nums[r]))
+                        break
+                    if total < target:
+                        l += 1
+                    if total > target:
+                        r -= 1
+        
+        return result
+                    
