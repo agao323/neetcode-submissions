edf32a62a8ac8ad930090cf9f04ab9084eefa3c2
agao323 <agao323@gmail.com>
2026-08-06 19:21:58 -0400

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        """
+        initial thoughts:
+            - maintain a sliding window of size len(s1)
+            - compare equivalency in dicts:
+                1. char counts for s1
+                2. char counts for current window
+        """
+        if not s1: 
+            return True
+            
+        if not s2:
+            return False
+
+        s1_counts = defaultdict(int)
+        for c1 in s1:
+            s1_counts[c1] += 1
+        
+        s2_counts = defaultdict(int)
+        for i in range(len(s1)):
+            s2_counts[s2[i]] += 1
+        
+        l, r = 0, len(s1)
+        while r < len(s2):
+            if s1_counts == s2_counts:
+                return True
+            
+            s2_counts[s2[l]] -= 1
+            if s2_counts[s2[l]] == 0:
+                del s2_counts[s2[l]]
+            l += 1
+            s2_counts[s2[r]] += 1
+            r += 1
+        
+        return s1_counts == s2_counts
