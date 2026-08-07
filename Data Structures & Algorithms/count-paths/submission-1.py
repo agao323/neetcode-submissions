eca3c1865c1c0cdd26bb17020ab94dc27e5d4217
agao323 <agao323@gmail.com>
2026-08-06 22:12:53 -0400

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        """
+        TIME
+            10:56.46 - 2D dp array
+
+        initial thoughts:
+            - initialize empty grid m x n
+            - dp. move down and right, each cell is just
+              paths stored in up and left cells + 1
+        """
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp[1][1] = 1
+
+        rows, cols = len(dp), len(dp[0])
+        for i in range(1, rows):
+            for j in range(1, cols):
+                dp[i][j] += dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[-1][-1]
+        
