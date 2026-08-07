319dcabd71ffa332355a1c4b48250d78250f2330
agao323 <agao323@gmail.com>
2026-08-06 22:13:16 -0400

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        """
+        TIME: 
+            39:37 - gave up and looked at solution
+
+        brute force:
+            - every * can be '(', '', or ')'
+            - generate every possibility and check if valid
+            - O(3^n) if every char is *, ie "***" gives 27 combinations to check
+
+        stack + backtracking?
+            - open parens '(' -> push onto stack
+            - close parens ')' -> try to pop and open parens from stack
+            - asterisk: explore all three options
+
+        any better optimizations? probably dp
+        """
+        
+        left_min, left_max = 0, 0
+
+        for c in s:
+            if c == '(':
+                left_min += 1
+                left_max += 1
+            elif c == ')':
+                left_min -= 1
+                left_max -= 1
+            else:
+                left_min -= 1
+                left_max += 1
+            if left_max < 0:
+                return False
+            if left_min < 0:
+                left_min = 0
+        return left_min == 0
