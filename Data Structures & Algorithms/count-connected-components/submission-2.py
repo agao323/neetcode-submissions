8951a3faea1c385e01ed55a350538d502cc977ae
agao323 <agao323@gmail.com>
2026-08-08 17:46:27 -0400

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,70 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        """
+        TIME: 7:13.97
+            - runtime is really bad (beats 0.96%), looking into optimizations
+
+        thoughts:
+            - build the adjacency matrix to represent the graph
+            - algorithm:
+                - maintain a set of seen nodes
+                - go through every node and traverse the CC
+                - add all the nodes to the seen set
+                - add 1 every time we finish going through every CC
+                - return result
+        """
+
+        """ DFS        
+        """
+        graph = defaultdict(list)
+        for a, b in edges:
+            # add both since it's undirected, although probably not necessary?
+            graph[a].append(b)
+            graph[b].append(a)
+
+        visited, count = set(), 0
+
+        def dfs(n: int) -> None:
+            if n in visited:
+                return
+            visited.add(n)
+            for neighbor in graph[n]:
+                dfs(neighbor)
+
+        for i in range(n):
+            if i in visited:
+                continue
+            dfs(i)
+            count += 1
+        
+        return count
+
+        """ BFS TIME 7:13.97
+
+        graph = defaultdict(list)
+        for a, b in edges:
+            # add both since it's undirected, although probably not necessary?
+            graph[a].append(b)
+            graph[b].append(a)
+
+        seen = set()
+        count = 0
+        for i in range(n):
+            # traverse the CC
+            if i in seen:
+                continue
+            
+            q = deque([i])
+            while q:
+                curr = q.popleft()
+                seen.add(curr)
+                for neighbor in graph[curr]:
+                    if neighbor not in seen:
+                        q.append(neighbor)
+
+            count += 1
+        
+        return count
+        """
