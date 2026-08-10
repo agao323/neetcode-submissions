fe40db86db21cf4146786926e887240d425ddb2d
agao323 <agao323@gmail.com>
2026-08-09 23:06:06 -0400

    Add: binary-tree-right-side-view - submission-2

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-2.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        TIME: 3:59.82
+        
+        level-order traversal, return last element in each level
+        """
+        if not root:
+            return []
+
+        level_order = self.levelOrder(root)
+        return [level[-1].val for level in level_order]
+    
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        queue = [root]
+        while queue:
+            result.append(queue)
+            next_level = []
+            for node in queue:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            queue = next_level
+        
+        return result
