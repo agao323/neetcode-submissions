8c254eb1739ed6536af4df54469d319e12b7907e
agao323 <agao323@gmail.com>
2026-08-11 18:08:45 -0400

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """
+        same thing as house robber I, but you can't rob the first and last houses
+
+        so same implementation but dp array is len(nums) - 1? but then that doesn't
+        even check the last house. use two dp arrays of len(nums) - 1? that should
+        work but maybe not the most efficient
+        """
+
+        dp1 = [0] * (len(nums) + 2)
+        dp2 = [0] * (len(nums) + 2)
+
+        for i in range(2, len(dp1) - 1):
+            dp1[i] = max(dp1[i - 1], dp1[i - 2] + nums[i - 2])
+
+        for j in range(3, len(dp2)):
+            dp2[j] = max(dp2[j - 1], dp2[j - 2] + nums[j - 2])
+        
+        return max(max(dp1), max(dp2))
