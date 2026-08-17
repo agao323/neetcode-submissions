616f2e289d83f80e5e3e115697c88833c37118b3
agao323 <agao323@gmail.com>
2026-08-17 19:20:30 -0400

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        """
+        can jump backwards
+
+        initial thoughts:
+            - dfs through the possible paths, which is jump forward or jump back
+            - base case: curr index < 0 or >= len(nums), we're out of bounds
+                return False
+            - base case: curr index == len(nums) - 1
+                return True
+            - dfs(curr - curr[i]), dfs(curr + curr[i])
+        """
+        seen = set()
+
+        def dfs(i):
+            if i in seen:
+                return False
+            seen.add(i)
+            if i >= len(nums) - 1:
+                return True
+            if i < 0 or nums[i] == 0:
+                return False
+            
+            return dfs(i + nums[i]) or dfs(i - nums[i])
+        
+        return dfs(0)
