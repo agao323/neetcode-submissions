2a6c3d68bd6f5fbf890be363bd16c31b5a2ae954
agao323 <agao323@gmail.com>
2026-08-17 18:40:23 -0400

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        """
+        start with creating prefix sum arry:
+            [2,-1,3,1,3,4,3,7]
+
+        iterate through this array.
+        at each point, track the smallest prefix sum
+        we've encountered, and track what the curr element
+        - the smallest sum would be. If it's positive, we
+        keep smallest sum to 0, because we should always
+        take a positive number.
+
+        the largest result will give us the right answer
+        """
+        if not nums:
+            return 0
+
+        prefix_sums = []
+        curr_sum = 0
+        for num in nums:
+            curr_sum += num
+            prefix_sums.append(curr_sum)
+        
+        print(prefix_sums)
+
+        min_sum = prefix_sums[0]
+        res = prefix_sums[0]
+        for ps in prefix_sums[1:]:
+            res = max(res, ps - min_sum)
+            min_sum = min(min_sum, ps)
+        
+        return res
