8b0f8ca4c9c4d3de7416aae73b7a2e686c509944
agao323 <agao323@gmail.com>
2026-08-27 16:58:03 -0400

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.end = True
+
+    def search(self, word: str) -> bool:
+        queue = [(self.root, 0)]
+        while queue:
+            cur, i = queue.pop(0)
+            if word[i] == '.':
+                for c, child in cur.children.items():
+                    if child.end and i == len(word) - 1:
+                        return True
+                    queue.append((child, i + 1))
+            else:
+                c = word[i]
+                if c in cur.children:
+                    if i == len(word) - 1 and cur.children[c].end:
+                        return True
+                    queue.append((cur.children[c], i + 1))
+
+        return False
+        
