857bb7346561fda173f11d70aacb2231d825145c
agao323 <agao323@gmail.com>
2026-08-27 15:56:00 -0400

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        """
+        TIME:
+            16:06
+            
+        Can apply the O(n^2) solution here where we expand outwards
+        any time we see a valid palindrome, count it
+
+        algo:
+            track num of substrings in result
+            iterate through each char in s
+                separate tracking for odd and even
+                expand outwards and increment if s[l] == s[r]
+            return result
+        """
+
+        result = 0
+
+        for i in range(len(s)):
+            # odd
+            l = r = i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                result += 1
+                l -= 1
+                r += 1
+            
+            # even
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                result += 1
+                l -= 1
+                r += 1
+            
+        return result
