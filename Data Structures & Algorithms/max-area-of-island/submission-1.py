ddc4a2e0785fedb1dec0d35aec397da507617970
agao323 <agao323@gmail.com>
2026-08-28 17:41:14 -0400

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        """
+        go through the matrix
+        if we encounter a 1, start a bfs
+            flip all 1s to 0s so we don't recount islands
+            maintain global largest island
+        return global largest
+
+        grid=[
+            [1,1,0,0,0],
+            [1,1,0,0,0],
+            [0,0,0,1,1],
+            [0,0,0,1,1]
+        ]
+
+        """
+
+        rows, cols = len(grid), len(grid[0])
+        result = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 0:
+                    continue
+
+                count = 0
+                queue = [(i, j)]
+                while queue:
+                    x, y = queue.pop(0)
+                    dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+                    if grid[x][y] == 0:
+                        continue
+
+                    grid[x][y] = 0
+                    count += 1
+
+                    for dx, dy in dirs:
+                        if 0 <= x + dx < len(grid) and grid[x + dx][y] == 1:
+                            queue.append((x + dx, y))
+                        if 0 <= y + dy < len(grid[0]) and grid[x][y + dy] == 1:
+                            queue.append((x, y + dy))
+
+                result = max(result, count)
+        
+        return result
+         
