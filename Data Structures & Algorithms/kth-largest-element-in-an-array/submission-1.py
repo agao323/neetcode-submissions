7666164d4411e82a8c1a39e441161db5383674ae
agao323 <agao323@gmail.com>
2026-08-28 17:22:10 -0400

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        """
+        TIME:
+            4:33.65 - same concept as question I just did
+            
+        maintain a min heap of size k, so the first element
+        is always the kth largest of what we've currently scanned
+
+        if we encounter an element larger than the top of the heap,
+        we insert it and pop until it's size k still
+        
+        return top of heap for O(nlogk) > O(nlogn)
+        """
+        min_heap = []
+
+        for num in nums:
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, num)
+            elif num < min_heap[0]:
+                continue
+            else:
+                heapq.heappush(min_heap, num)
+                while len(min_heap) > k:
+                    heapq.heappop(min_heap)
+        
+        return min_heap[0]
