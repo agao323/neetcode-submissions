226f08c3098696f58e870227e2828ba008c88ccb
agao323 <agao323@gmail.com>
2026-08-31 17:01:56 -0400

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        """
+        maintain a high water mark, ie farthest right we can go
+        scan through the array
+
+        anytime the high water mark increases, increment by 1
+
+        keep going until we reach len(nums) - 1
+        """
+
+        right = 0
+        result = 0
+        for i in range(len(nums)):
+            cur = i + nums[i]
+            if cur > right:
+                result += 1
+                right = cur
+            if cur >= len(nums) - 1:
+                return result
+                
+        return result
